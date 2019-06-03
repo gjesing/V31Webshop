d228405b136f12ec9c3af1b62a89147becd59489
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-06-03 15:01:33 +0200

    Header and nav created

--- index.php
+++ index.php
@@ -5,9 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Webshop</title>
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Roboto+Condensed&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <header>
+        <div class="logo">
+            <div></div>
+            <h1>Kawaii Stationery</h1>
+        </div>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Forside</a></li>
+            <li><a href="#">Produkter</a></li>
+            <li><a href="#">Om os</a></li>
+            <li><a href="#">Log ind</a></li>
+        </ul>
+    </nav>
 </body>
 </html>
