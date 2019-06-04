7cd0679c12f9965f9a483e8f413927b86392a452
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-06-04 12:01:03 +0200

    Main layout created

--- index.php
+++ index.php
@@ -6,12 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Webshop</title>
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Roboto+Condensed&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <header>
+    <nav>
+        <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Log ind</a></li>
+        </ul>
+    </nav>
         <div class="logo">
-            <div></div>
+            <img src="img/logo.png" alt="Logo">
             <h1>Kawaii Stationery</h1>
         </div>
     </header>
@@ -20,8 +27,34 @@
             <li><a href="#">Forside</a></li>
             <li><a href="#">Produkter</a></li>
             <li><a href="#">Om os</a></li>
-            <li><a href="#">Log ind</a></li>
+            <li><a href="#">Indkøbskurv<i class="fas fa-shopping-cart"></i></a></li>
         </ul>
     </nav>
+    <main>
+        <article>
+            <img src="img/MeowCatGelPen.jpg" alt="">
+            <h2>Meow Cat Gel Pen</h2>
+            <h3>50 kr.</h3>
+            <button>Læg i kurv</button>
+        </article>
+        <article>
+            <img src="img/CatsDogMemoPads.jpg" alt="">
+            <h2>Cats & Dogs Memo Pads</h2>
+            <h3>50 kr.</h3>
+            <button>Læg i kurv</button>
+        </article>
+        <article>
+            <img src="img/MiniVitaminShapedHighlighterPens.jpg" alt="">
+            <h2>Mini Vitamin Shaped Highlighter Pens</h2>
+            <h3>50 kr.</h3>
+            <button>Læg i kurv</button>
+        </article>
+        <article>
+            <img src="img/MilkCartonPencilCase.jpg" alt="">
+            <h2>Milk Carton Pencil Case</h2>
+            <h3>50 kr.</h3>
+            <button>Læg i kurv</button>
+        </article>
+    </main>
 </body>
 </html>
