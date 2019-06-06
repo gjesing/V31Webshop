06b25f34db05e1aaa56fe154b3256af8857b5fdd
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-06-06 15:00:53 +0200

    Layout improved

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Her kan du købe alverdens kawaii stationery til billige priser. Alle produkter er importeret direkte fra Japan.">
     <title>Webshop</title>
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Roboto+Condensed&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
@@ -31,30 +32,41 @@
         </ul>
     </nav>
     <main>
-        <article>
-            <img src="img/MeowCatGelPen.jpg" alt="">
-            <h2>Meow Cat Gel Pen</h2>
-            <h3>50 kr.</h3>
-            <button>Læg i kurv</button>
-        </article>
-        <article>
-            <img src="img/CatsDogMemoPads.jpg" alt="">
-            <h2>Cats & Dogs Memo Pads</h2>
-            <h3>50 kr.</h3>
-            <button>Læg i kurv</button>
-        </article>
-        <article>
-            <img src="img/MiniVitaminShapedHighlighterPens.jpg" alt="">
-            <h2>Mini Vitamin Shaped Highlighter Pens</h2>
-            <h3>50 kr.</h3>
-            <button>Læg i kurv</button>
-        </article>
-        <article>
-            <img src="img/MilkCartonPencilCase.jpg" alt="">
-            <h2>Milk Carton Pencil Case</h2>
-            <h3>50 kr.</h3>
-            <button>Læg i kurv</button>
-        </article>
+        <section>
+            <h1>Velkommen til Kawaii Stationery</h1>
+            <p>Her kan du købe alverdens kawaii stationery til billige priser. Alle produkter er importeret direkte fra Japan.</p>
+        </section>
+        <section>
+            <h1>Nye produkter</h1>
+            <article>
+                <img src="img/MeowCatGelPen.jpg" alt="Meow Cat Gel Pen">
+                <h2>Meow Cat Gel Pen</h2>
+                <p>What’s more adorable than a kitten? Not much! Which is why we love this collection of cute kitten topped gel pens. The perfect addition to your cute stationery collection. Each pen includes its own unique adorable face.</p>
+                <h3>20 kr.</h3>
+                <button>Læg i kurv</button>
+            </article>
+            <article>
+                <img src="img/CatsDogMemoPads.jpg" alt="">
+                <h2>Cats & Dogs Memo Pads</h2>
+                <p>Take home one of our adorable Cat or Dog memo pads. The cute Dog and Cat designs are perfect for leaving notes or add them to notebooks and bullet journals for a little dose of cute. Each pad includes 30 pages.</p>
+                <h3>20 kr.</h3>
+                <button>Læg i kurv</button>
+            </article>
+            <article>
+                <img src="img/MiniVitaminShapedHighlighterPens.jpg" alt="">
+                <h2>Mini Vitamin Shaped Highlighter Pens</h2>
+                <p>Chuck out your boring old highlighters, and welcome this cute little family of Vitamin shaped mini highlighter pens into your pencil case. Each pen has its own unique personality. The perfect size to pop into any bag and go.</p>
+                <h3>30 kr.</h3>
+                <button>Læg i kurv</button>
+            </article>
+            <article>
+                <img src="img/MilkCartonPencilCase.jpg" alt="">
+                <h2>Milk Carton Pencil Case</h2>
+                <p>We love these novelty Milk Carton shaped pencil cases. Featuring cute and adorable characters that will be sure to brighten your day. The unique designs means there is plenty of space to store all your pens and pencils. Available in three lovable designs.</p>
+                <h3>40 kr.</h3>
+                <button>Læg i kurv</button>
+            </article>
+        </section>
     </main>
 </body>
 </html>
