1a292fc7d767f4891dd2230e6c8007fc0937c027
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-06-13 14:30:30 +0200

    When a product is created the userId of the product will now be the userId of the user who creates it (the user logged in)

--- assets/createProduct.php
+++ assets/createProduct.php
@@ -1,13 +1,15 @@
 <?php
+session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {            
     require "connect.php";
     try {
-        $statement = $dbh->prepare("INSERT INTO Products (name, description, price, image, categoryId, userId) VALUES (:name, :description, :price, :image, :categoryId, 1)");
+        $statement = $dbh->prepare("INSERT INTO Products (name, description, price, image, categoryId, userId) VALUES (:name, :description, :price, :image, :categoryId, :userId)");
         $statement->bindParam(':name', $_POST['name']);
         $statement->bindParam(':description', $_POST['description']);
         $statement->bindParam(':price', $_POST['price']);
         $statement->bindParam(':image', $_POST['image']);
         $statement->bindParam(':categoryId', $_POST['categoryId']);
+        $statement->bindParam(':userId', $_SESSION['userId']);
         $statement->execute();
         header("location: ../products.php");
     } catch (PDOException $e) {
