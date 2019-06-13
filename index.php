b46fe94340fb77f1b231e1698bd32e3b62aa6bef
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-06-13 18:19:25 +0200

    Users will now get a personalized welcome message, if they are signed in

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ session_start();
     </nav>
     <main>
         <section>
-            <h1>Velkommen til Kawaii Stationery</h1>
+            <h1>Velkommen <?php echo (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']) ? $_SESSION['firstName'] : "til Kawaii Stationery"; ?></h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit laborum sunt non porro deserunt, modi molestiae omnis iste eligendi, odit, explicabo unde rerum pariatur maiores eius amet et laudantium commodi!</p>
         </section>
         <section  class="products">
