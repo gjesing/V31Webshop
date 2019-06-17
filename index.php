56c9b80e6a9c01aad33112a51f147589f0593bcd
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-06-17 09:29:45 +0200

    Welcome message in index.php changed

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include "header.php";
 ?>
 <section>
     <h1>Velkommen <?php echo (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']) ? $_SESSION['firstName'] : "til Kawaii Stationery"; ?></h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit laborum sunt non porro deserunt, modi molestiae omnis iste eligendi, odit, explicabo unde rerum pariatur maiores eius amet et laudantium commodi!</p>
+    <p>Her kan du købe alverdens kawaii stationery til billige priser. Alle produkter er importeret direkte fra Japan.</p>
 </section>
 <section class="products">
     <h1>Nye produkter</h1>
